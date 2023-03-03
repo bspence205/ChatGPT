164a266cbd1e646fdebc14e08279d382d3ae6736
bspence205 <brianspencer@spencerbrian.com>
2023-03-03 06:15:08 -0800

    updatedlayout to include responsive layout of the sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,11 @@ export default function RootLayout({
         <div className='flex'>
 
           {/* SideBar */}
-
+          <div className='bg-[#202123] max=w=xs h-screen
+          overflow-y-auto md:min-w-[20rem]'>
           <SideBar />
+          </div>
+          
 
 
           {/* ClientProvider - Notification */}
